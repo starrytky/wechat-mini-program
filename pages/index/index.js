253e187a9fbb646ed906f7cd43c0604fe420b788
home-starrytky <starrytky@qq.com>
2020-08-16 08:59:46 +0800

    push code

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,25 +1,29 @@
-//index.js
-//获取应用实例
+// pages/index01/index.js
 const app = getApp()
-
 Page({
+
+  /**
+   * 页面的初始数据
+   */
   data: {
-    motto: 'Hello World',
+    welcome: '开启小程序',
     userInfo: {},
     hasUserInfo: false,
     canIUse: wx.canIUse('button.open-type.getUserInfo')
   },
 
-  onLoad: function () {
-    if (app.globalData.userInfo) {
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    if(app.globalData.userInfo){
       this.setData({
         userInfo: app.globalData.userInfo,
         hasUserInfo: true
       })
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+    // } else if(this.data.canIUse){
+      //由于 getUserInfo 是网络请求，可能会在Page.onLoad之后才返回
       // 所以此处加入 callback 以防止这种情况
-      console.log(this.data.canIUse)
       // app.userInfoReadyCallback = res => {
       //   this.setData({
       //     userInfo: res.userInfo,
@@ -27,24 +31,28 @@ Page({
       //   })
       // }
     } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
+      // 在没有 open-type=getUserInfo 版本兼容处理
       wx.getUserInfo({
         success: res => {
-          app.globalData.userInfo = res.userInfo
+          // app.globalData.userInfo = res.userInfo
           this.setData({
             userInfo: res.userInfo,
             hasUserInfo: true
-          })
+          }) 
         }
       })
     }
   },
   getUserInfo: function(e) {
-    console.log(e)
     app.globalData.userInfo = e.detail.userInfo
     this.setData({
       userInfo: e.detail.userInfo,
       hasUserInfo: true
+    })
+  },
+  handlehello(){
+    wx.switchTab({
+      url: '/pages/list/list',
     })
   }
 })
